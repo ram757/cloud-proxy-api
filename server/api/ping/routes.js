a1a6623d9bdb29e3a37dd19dc446ad757867a9f9
ram757 <rmcdonald144@gmail.com>
2019-03-07 11:03:03 -0500

    fix stuff

--- server/api/ping/routes.js
+++ server/api/ping/routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const ping = require('./logic/ping');
 
+const azure_sdk = require('azure');
+
 /* GET - /
     Ping endpoint to determine if service is running and if 
         database connection can be established.
@@ -18,7 +20,21 @@ router.get('/', function(req, res, next) {
     res.status(error.status);
     res.json(error);
   })
+});
 
+router.get('/azure', function(req, res, next) {
+  azure_sdk.
+
+    // ping.buildPingResponseData().then(function (pingResponseData) {
+    //     res.send(pingResponseData);
+    // }).catch(function (error) {
+    //
+    //     //ADD LOGGER
+    //     console.log("ERROR | ROUTER - GET - / : " + error.message);
+    //
+    //     res.status(error.status);
+    //     res.json(error);
+    // })
 });
 
 module.exports = router;
